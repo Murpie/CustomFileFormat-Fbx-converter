f8e5656d568f72f76eb043baac27ad88069e7a1c
EMIL HALLIN <emhj16@student.bth.se>
2018-04-05 09:40:40 +0200

    TestCube added

--- CustomFormat/main.cpp
+++ CustomFormat/main.cpp
@@ -3,6 +3,25 @@
 int main()
 {
 	printf("hello");
-	getchar();
+
+	FbxManager* manager = FbxManager::Create();
+
+	FbxScene::Create(manager, "");
+
+	FbxIOSettings* ios = FbxIOSettings::Create(manager, IOSROOT);
+	manager->SetIOSettings(ios);
+
+	FbxImporter* fbximporter = FbxImporter::Create(manager, "");
+
+	const char* filename = "testCube.fbx";
+
+	if (!fbximporter->Initialize(filename, -1, manager->GetIOSettings()))
+	{
+		printf("Call to fbximporter::initialize failed.\n");
+		printf("Error returned: %s\n\n", fbximporter->GetStatus().GetErrorString());
+		getchar();
+		exit(-1);
+	}
+
 	return 0;
 }
