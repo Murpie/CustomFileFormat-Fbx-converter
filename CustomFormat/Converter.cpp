52a67a24d9776b7fdbb33b8e961812fad05b6d63
EMIL HALLIN <emhj16@student.bth.se>
2018-04-12 12:08:00 +0200

    Positions, normals & UVs

--- CustomFormat/Converter.cpp
+++ CustomFormat/Converter.cpp
@@ -126,21 +126,68 @@ void Converter::exportFile2()
 	Vertex *vertices = new Vertex[counter.vertexCount];
 
 	std::vector<FbxVector4> pos;
+	std::vector<FbxVector4> norm;
+	std::vector<FbxVector2> uv;
+	FbxVector4 temp;
+	FbxVector2 tempUv;
+	
+
+	bool ItIsFalse = false;
+
+	printf("Mesh: %s\n", child->GetName());
 
 	int i = 0;
 	for (int polygonIndex = 0; polygonIndex < polygonCount; polygonIndex++)
 	{
 		for (int vertexIndex = 0; vertexIndex < mesh->GetPolygonSize(polygonIndex); vertexIndex++)
 		{
+			//Positions
 			pos.push_back(controlPoints[mesh->GetPolygonVertex(polygonIndex, vertexIndex)]);
 
-			//std::cout << i << "  x: " << vertices[i].x << "  y: " << vertices[i].y << "  z: " << vertices[i].z << std::endl;
-			//printf("Vertex[%d]: %f %f %f\n", i, (char)pos[i]);
+			//Normals
+			mesh->GetPolygonVertexNormal(polygonIndex, vertexIndex, temp);
+			norm.push_back(temp);
+
+			//UVs
+			FbxStringList uvSetNamesList;
+			mesh->GetUVSetNames(uvSetNamesList);
+
+			const char* uvNames = uvSetNamesList.GetStringAt(0);
+
+			mesh->GetPolygonVertexUV(polygonIndex, vertexIndex, uvNames, tempUv, ItIsFalse);
+			
+			uv.push_back(tempUv);
+
+			printf("Vertex[%d]: %f %f %f\n", i, pos[i][0], pos[i][1], pos[i][2]);
+			printf("Normal[%d]: %f %f %f\n", i, norm[i][0], norm[i][1], norm[i][2]);
+			printf("UV[%d]:     %f %f\n\n", i, uv[i][0], uv[i][1]);
+
+			vertices[i].x = pos[i][0];
+			vertices[i].y = pos[i][1];
+			vertices[i].z = pos[i][2];
+
+			vertices[i].nx = norm[i][0];
+			vertices[i].ny = norm[i][1];
+			vertices[i].nz = norm[i][2];
+
+			vertices[i].u = uv[i][0];
+			vertices[i].v = uv[i][1];
+
 			i++;
 		}
 	}
-	//std::cout << pos[0] << std::endl;
-	getchar();
+	
+	std::ofstream outfile("testt.leap", std::ofstream::binary);
+
+	outfile.write((const char*)&counter, sizeof(Counter));
+	outfile.write((const char*)vertices, sizeof(Vertex)*counter.vertexCount);
+
+	outfile.close();
+
+	if (child->GetChildCount() > 0)
+	{
+		loadMesh(child);
+	}
 }
 
 void Converter::printName(FbxMesh* meshName)
--- LibraryTest123/LibraryTest123/main.cpp
+++ LibraryTest123/LibraryTest123/main.cpp
@@ -23,14 +23,12 @@ int main() {
 
 	for (int i = 0; i < counterReader.vertexCount; i++)
 	{
-		std::cout << i << "|  X: " << vertices[i].x << ", Y: " << vertices[i].y << ", Z: " << vertices[i].z << std::endl;
-		std::cout << i << "| NX: " << vertices[i].nx << ", NY: " << vertices[i].ny << ", NZ: " << vertices[i].nz << std::endl << std::endl;
+		std::cout << i << "|  X: " << vertices[i].x << " " << vertices[i].y << " " << vertices[i].z << std::endl;
+		std::cout << i << "| NX: " << vertices[i].nx << " " << vertices[i].ny << " " << vertices[i].nz << std::endl;
+
+		printf("%d| UV: %f %f\n\n", i, vertices[i].u, vertices[i].v);
+
 	}
-	
-	std::cout << "Vertex count: " << counterReader.vertexCount << std::endl << std::endl;
-	std::cout << "Vtx on 0" << "   x: " << vertices[0].x << "   y: " << vertices[0].y << "   z: " << vertices[0].z << std::endl;
-	std::cout << "Vtx on 1" << "   x: " << vertices[1].x << "   y: " << vertices[1].y << "   z: " << vertices[1].z << std::endl;
-	std::cout << "Vtx on 15" << "   x: " << vertices[15].x << "   y: " << vertices[15].y << "   z: " << vertices[15].z << std::endl;
 
 	getchar();
 
