89360c53b995ec711bfca1f2f16eac06b0afaaa2
Emil <Emil@DESKTOP-2FRK7JH>
2018-04-27 10:00:37 +0200

    Level Exporter

--- CustomFormat/main.cpp
+++ CustomFormat/main.cpp
@@ -6,9 +6,9 @@ int main()
 {
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 	
-	Converter converter("tLVL.fbx");
+	Converter converter("Player_temp.fbx");
 
-	converter.isLevel = true; // change this when loading levels
+	converter.isLevel = false; // change this when loading levels
 
 	converter.importMesh();
 
