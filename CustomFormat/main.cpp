11d5ee779a7a10b930045b1bbf9286df2d8e49b3
EMIL HALLIN <emhj16@student.bth.se>
2018-05-14 15:43:29 +0200

    .

--- CustomFormat/main.cpp
+++ CustomFormat/main.cpp
@@ -8,7 +8,7 @@ int main()
 	
 	Converter converter("Campfire_bitang.fbx");
 
-	Cliffside_4.isLevel = false; // change this when loading levels
+	converter.isLevel = false; // change this when loading levels
 
 	converter.importMesh();
 
