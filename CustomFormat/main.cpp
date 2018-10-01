8813fca12b373bd561f0fee9b366a27245d810f8
PETTER FLOOD <pefl16@student.bth.se>
2018-10-01 16:18:39 +0200

    Fixed

--- CustomFormat/main.cpp
+++ CustomFormat/main.cpp
@@ -6,7 +6,7 @@ int main()
 {
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 	
-	Converter converter("test_file_name.fbx");
+	Converter converter("123.fbx");
 
 	converter.isLevel = false; // change this when loading levels
 	converter.importMesh();
